644a974ab1a9cc30ee94999ecc64f0be2d0a4f63
YuukiMachida <c0b2213430@edu.teu.ac.jp>
2023-06-20 16:56:29 +0900

    演習課題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,8 +4,10 @@ import random
 
 WIDTH, HEIGHT = 1600, 900
 
+# 入力キーの辞書
 key_dict = {pg.K_UP:(0, -5), pg.K_DOWN:(0, +5), pg.K_LEFT:(-5, 0),pg.K_RIGHT:(+5,0)}
 
+# 枠外に出ていないかを判断する関数
 def check_bound(rect: pg.rect)-> tuple[bool, bool]:
     yoko, tate = True , True
     if rect.left < 0 or WIDTH < rect.right:
@@ -13,6 +15,8 @@ def check_bound(rect: pg.rect)-> tuple[bool, bool]:
     if rect.top < 0 or HEIGHT < rect.bottom:
         tate = False
     return yoko ,tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -22,7 +26,9 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_img_flip = pg.transform.flip(kk_img, True, False)
     
-    # いろいろなこうかとんが入った辞書
+    kk_img_pien = pg.image.load("ex02/fig/1.png")
+    kk_img_pien = pg.transform.rotozoom(kk_img_pien, 0, 2.0)
+    # いろいろなこうかとんが入った辞書  
     kk_img_dict = {
         (0,0): pg.transform.rotozoom(kk_img, 0,1.0),
         (0, -5): pg.transform.rotozoom(kk_img_flip, 90,1.0),
@@ -34,6 +40,7 @@ def main():
         (-5, 0): pg.transform.rotozoom(kk_img, 0,1.0),
         (-5, -5): pg.transform.rotozoom(kk_img, -45,1.0),
         }
+    
     kk_rct = kk_img.get_rect()
     kk_rct.center = [900, 400]
     bom_r = pg.Surface((20, 20))
@@ -54,13 +61,19 @@ def main():
     tmr = 0
     vx = 5
     vy = 5
-    # print(f"{bom_rx},{bom_ry}")
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
         if kk_rct.colliderect(bom_rct):
             print("ゲームオーバー")
+            screen.blit(bg_img, [0, 0])
+            screen.blit(kk_img_pien, kk_rct)
+            pg.display.update()
+            clock.tick(0.3)
             return
 
         bom_r = bom_rs[min(tmr//500, 9)]
