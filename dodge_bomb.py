279ecb9f2addb7d18f6a57adfef88eeed3ab29d1
YuukiMachida <c0b2213430@edu.teu.ac.jp>
2023-06-20 14:11:09 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,9 +16,13 @@ def main():
     bom_r.set_colorkey((0, 0, 0))
     bom_rx = random.randint(0, WIDTH - 20)
     bom_ry = random.randint(0, HEIGHT - 20)
+    bom_rct = bom_r.get_rect()
+    bom_rct.center = bom_rx, bom_ry
     clock = pg.time.Clock()
     tmr = 0
-    print(f"{bom_rx},{bom_ry}")
+    vx = 5
+    vy = 5
+    # print(f"{bom_rx},{bom_ry}")
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -26,10 +30,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bom_r, [bom_rx, bom_ry])
+        screen.blit(bom_r, bom_rct)
+        bom_rct.move_ip((vx,vy))
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
