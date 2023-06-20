b6aa6b47de34e1690171291aafde192c85952209
YuukiMachida <c0b2213430@edu.teu.ac.jp>
2023-06-20 16:07:33 +0900

    演習課題１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -20,6 +20,18 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_flip = pg.transform.flip(kk_img, True, False)
+    kk_img_dict = {
+        (0,0): pg.transform.rotozoom(kk_img, 0,1.0),
+        (0, -5): pg.transform.rotozoom(kk_img_flip, 90,1.0),
+        (5, -5): pg.transform.rotozoom(kk_img_flip, 45,1.0),
+        (5, 0): pg.transform.rotozoom(kk_img_flip, 0,1.0),
+        (5, 5): pg.transform.rotozoom(kk_img_flip, -45,1.0),
+        (0, 5): pg.transform.rotozoom(kk_img_flip, -90,1.0),
+        (-5, +5): pg.transform.rotozoom(kk_img, 45,1.0),
+        (-5, 0): pg.transform.rotozoom(kk_img, 0,1.0),
+        (-5, -5): pg.transform.rotozoom(kk_img, -45,1.0),
+        }
     kk_rct = kk_img.get_rect()
     kk_rct.center = [900, 400]
     bom_r = pg.Surface((20,20))
@@ -43,17 +55,21 @@ def main():
             print("ゲームオーバー")
             return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
-        screen.blit(bom_r, bom_rct)
-        bom_rct.move_ip((vx,vy))
         key_lst = pg.key.get_pressed()
         sum_mv = [0 ,0]
         for k, mv in key_dict.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        
+        
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img_dict[tuple(sum_mv)], kk_rct)
+        screen.blit(bom_r, bom_rct)
+
         kk_rct.move_ip(sum_mv)
+        bom_rct.move_ip((vx,vy))
+        
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         yoko, tate = check_bound(bom_rct)
